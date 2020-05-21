84f10a46745173952ea643ad0de59072fa5664ea
Andrade Tamburus <dots@gft.com>
2020-05-21 02:25:24 -0300

    first commit

---
+++ debug-es6/.compiled/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var _math = require('./math');
+
+var num1 = 5,
+    num2 = 10;
+
+console.log('Add: ', (0, _math.add)(num1, num2));
+console.log('Multiply: ', (0, _math.multiply)(num1, num2));
+//# sourceMappingURL=app.js.map
